7be1d676e24879a52ea2857895406a679c4155b7
IvanPalmar0207 <palmar.ivan0205@gmail.com>
2023-11-17 10:53:20 -0500

    usuariosCRUD-IvanPalmar

---
+++ primeraAplicacion/primeraAplicacion/app/src/main/java/com/example/primeraaplicacion/solicitarActualizar.kt
@@ -0,0 +1,11 @@
+package com.example.primeraaplicacion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class solicitarActualizar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_solicitar_actualizar)
+    }
+}
