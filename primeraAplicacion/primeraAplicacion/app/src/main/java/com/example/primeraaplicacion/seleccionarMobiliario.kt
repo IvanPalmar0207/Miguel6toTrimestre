00548a3f08f77b0791da59de4da0bc5c41de6353
IvanPalmar0207 <palmar.ivan0205@gmail.com>
2023-11-22 11:24:49 -0500

    ActualizacionHab-IvanPalmar

---
+++ primeraAplicacion/primeraAplicacion/app/src/main/java/com/example/primeraaplicacion/seleccionarMobiliario.kt
@@ -0,0 +1,11 @@
+package com.example.primeraaplicacion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class seleccionarMobiliario : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seleccionar_mobiliario)
+    }
+}
