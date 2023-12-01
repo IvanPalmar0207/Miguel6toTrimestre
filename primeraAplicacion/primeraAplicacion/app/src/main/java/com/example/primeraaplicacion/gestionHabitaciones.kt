208661073d13cdde5845912031176254a8595bac
IvanPalmar0207 <palmar.ivan0205@gmail.com>
2023-12-01 06:42:07 -0500

    Habitacion-IvanPalmar

---
+++ primeraAplicacion/primeraAplicacion/app/src/main/java/com/example/primeraaplicacion/gestionHabitaciones.kt
@@ -0,0 +1,11 @@
+package com.example.primeraaplicacion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class gestionHabitaciones : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gestion_habitaciones)
+    }
+}
